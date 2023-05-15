0be48b9a6fecd06da41f1124c2612fe25c45d07d
kao19 <feiyukine@gmail.com>
2023-05-15 18:49:12 +0100

    feat: adding text to speech

---
+++ Assets/Scripts/TTS_ocean_1.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TTS_ocean_1 : MonoBehaviour
+{
+
+    public AudioSource _audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audio = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator DownloadTheAudio() {
+        string uri = string.Format("https://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&q={0}&tl=Ar-gb","ﺣﻴﻮﺍﻧﺎﺕ ﻣﺘﻨﻮﻋﺔ ﺑﺤﺮﻳﺔ");
+        
+        WWW www = new WWW(uri);
+        yield return www;
+
+        _audio.clip = www.GetAudioClip(false, true, AudioType.MPEG);
+        _audio.Play();
+    }
+
+    public void ButtonClick() {
+        StartCoroutine(DownloadTheAudio());
+    }
+
+}
